9619c5aaccbef3a331c337d81031723f6d055a83
Louis Githogori Mariara <louismariara@gmail.com>
2024-12-11 14:41:51 +0300

    added game logic

--- mancala.py
+++ mancala.py
@@ -2,3 +2,12 @@ import datetime
 #array of beads in each mancala and pit
 ar_ray= [4,4,4,4,4,4,0,4,4,4,4,4,4,0] 
 player_onez_turn= True
+def add_beads_to_consecutive_pits(current_index):
+    bead_count= int(ar_ray[current_index])
+    # re-assign the value of the current ar_ray element
+    ar_ray[current_index]= 0 
+    for consecutive_index in range(1, (bead_count + 1)):
+        #this👇 prevents an IndexError that occurs when consecutive_index is greater than the ar_ray length
+        target_index = (current_index + consecutive_index) % len(ar_ray)
+        # add 1 to each of the consecutive pits upto the bead_count
+        ar_ray[target_index] = int(ar_ray[target_index]) + 1
