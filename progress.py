c731c2180b4893d97a1e3c5248fe3e57042a9e86
jobwayne <otienojobwayneotieno@gmail.com>
2024-12-18 09:36:44 +0300

    SPMG-3 wayne

---
+++ progress.py
@@ -0,0 +1,10 @@
+def save_record():
+    x = datetime.datetime.now()
+
+    # write the current date and time to the scores text file
+    # write the last board rendered plus players scores  
+    with open('scores.txt', 'a') as file:
+        file.write(f"\n{x.strftime('%I')}:{x.strftime('%M')} {x.strftime('%p')}\n")
+        file.write(f"{x.strftime('%d')}th {x.strftime('%B')}, {x.strftime('%Y')}\n")
+        file.write(print_board())
+        file.write(print_score())
